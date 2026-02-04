645b670453781a436c540e584233d1041fa5b6fb
Abhi Raj <abhiraj.jayaraj31@gmail.com>
2026-02-04 11:21:36 +0530

    Add menu imgaes

--- index.js
+++ index.js
@@ -8,21 +8,25 @@ app.use(express.json());
 const menuItems = [
   {
     name: "Chicken Biryani",
-    description: "Aromatic basmati rice with slow-cooked spices",
-    price: "₹180",
-    category: "Rice",
+  description: "Aromatic basmati rice with slow-cooked spices",
+  price: 180,
+  category: "Rice",
+  image: "https://images.unsplash.com/photo-1604908177522-040c1c6e0b87"
   },
   {
     name: "Paneer Butter Masala",
     description: "Creamy tomato gravy with soft paneer cubes",
-    price: "₹160",
+    price: "160",
     category: "Curry",
+    image: "https://images.unsplash.com/photo-1604908177522-040c1c6e0b87"
   },
+  
   {
     name: "Grilled Chicken",
     description: "Smoky, juicy, and perfectly seasoned",
-    price: "₹220",
+    price: "220",
     category: "Grill",
+    image: "https://images.unsplash.com/photo-1604908177522-040c1c6e0b87"
   },
 ];
 
